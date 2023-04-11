a38640fbfc9c1c823a32461718669df61aac8fe6
Rafiul-Islam <rafi.cse.ru@gmail.com>
2023-04-11 23:42:11 +0600

    feature added - create user with unique email

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const User = require('../models/User');
 
 // create a user
-router.post('/', [
+router.post('/user', [
     body('name', "Enter a valid name!").isLength({min: 3}).notEmpty(),
     body('email', "Enter a valid email!").isEmail().notEmpty(),
     body('password', "Password length must be 6 to 12 characters!").isLength({min: 6, max: 12}).notEmpty(),
@@ -15,13 +15,22 @@ router.post('/', [
         return response.status(400).json({errors: errors.array()});
     }
 
-    User.create({
-        name: request.body.name,
-        email: request.body.email,
-        password: request.body.password,
-    })
-        .then(user => response.json(user))
-        .catch(ex => console.log(ex.message));
+    // check whether the user with this email exist already
+    try {
+        let user = await User.findOne({email: request.body.email});
+        if (user)
+            return response.status(400).json({errors: "Invalid User Credential!"});
+
+        user = await User.create({
+            name: request.body.name,
+            email: request.body.email,
+            password: request.body.password,
+        });
+        response.send(user);
+    } catch (e) {
+        console.error(e.message);
+        response.status(500).json({errors: "Something unexpected happened!"});
+    }
 });
 
 module.exports = router;
