59bf320ecf2762960d366c4cb911165d27e5d9a3
Rafiul-Islam <rafi.cse.ru@gmail.com>
2023-04-12 09:53:31 +0600

    build a login api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -23,7 +23,7 @@ router.post('/user', [
         // check whether the user with this email exist already
         let user = await User.findOne({email: request.body.email});
         if (user)
-            return response.status(400).json({errors: "Invalid User Credential!"});
+            return response.status(400).json({errors: "User with this email already exist!"});
 
         const salt = await bcrypt.genSalt(10);
         const securedPassword = await bcrypt.hash(request.body.password, salt);
@@ -42,10 +42,48 @@ router.post('/user', [
         }
 
         const authToken = jwt.sign(data, JWT_SECRET);
-
         response.json({authToken});
-    } catch (e) {
-        console.error(e.message);
+    } catch (error) {
+        console.error(error.message);
+        response.status(500).json({errors: "Something unexpected happened!"});
+    }
+});
+
+// Authenticate a user
+router.post('/login', [
+    body('email', "Enter a valid email!").isEmail().notEmpty(),
+    body('password', "Password length must be 6 to 12 characters!").isLength({min: 6, max: 12}).notEmpty(),
+], async (request, response) => {
+
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        return response.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = request.body;
+    try {
+        let user = await User.findOne({email});
+        if (!user)
+            return response.status(400).json({errors: "Please try to login with valid credential!"});
+
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+
+        if (!passwordCompare)
+            return response.status(400).json({errors: "Please try to login with valid credential!"});
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+        response.json({authToken});
+
+    } catch (error) {
+        console.error(error.message);
         response.status(500).json({errors: "Something unexpected happened!"});
     }
 });
